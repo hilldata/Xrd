698833e9d449a79acd40fe3d34f447d3bccd3635
John Hill <hilldata@hotmail.com>
2020-03-27 11:44:03 -0400

    Started Tests project

---
+++ Xrd.CommonTests/Collections/CollectionExtensionsTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Xrd.Collections.Tests {
+	[TestClass()]
+	public class CollectionExtensionsTests {
+		[TestMethod()]
+		public void IsNullOrEmpty_TestNull() {
+			// Arrange
+			List<object> list = null;
+			object[] vs = null;
+
+			// Assert
+			Assert.IsTrue(list.IsNullOrEmpty());
+			Assert.IsTrue(vs.IsNullOrEmpty());
+		}
+
+		[TestMethod()]
+		public void IsNullOrEmpty_TestEmpty() {
+			// Arrange
+			List<object> list = new List<object>();
+			object[] vs = new object[0];
+
+			// Assert
+			Assert.IsTrue(list.IsNullOrEmpty());
+			Assert.IsTrue(vs.IsNullOrEmpty());
+		}
+
+		[TestMethod()]
+		public void IsNullOrEmpty_TestHasItems() {
+			// Arrange
+			List<object> list = new List<object>() { 1, 2, "string" };
+			object[] vs = new object[] { 1, 2, 3 };
+
+			// Assert
+			Assert.IsFalse(list.IsNullOrEmpty());
+			Assert.IsFalse(vs.IsNullOrEmpty());
+		}
+		[TestMethod()]
+		public void AddIfNotNull_TestExceptions() {
+			// Arrange
+			List<object> list = null;
+
+			// Test null source throws exception
+			Assert.ThrowsException<ArgumentNullException>(() => list.AddIfNotNull(1));
+		}
+
+		[TestMethod()]
+		public void AddIfNotNull_TestAdding() {
+			// Arrange
+			var list = new List<object>();
+			object item = null;
+
+			// Test null is not added.
+			Assert.IsFalse(list.AddIfNotNull(item));
+
+			// Arrange
+			item = 1;
+			// Test not null is added
+			Assert.IsTrue(list.AddIfNotNull(item));
+
+			// Test enforceUnique: true => not added
+			Assert.IsFalse(list.AddIfNotNull(item, true));
+
+			// Test enforceUnique: false => duplicate added
+			Assert.IsTrue(list.AddIfNotNull(item));
+
+			// Make sure list count is now 2
+			Assert.AreEqual(2, list.Count);
+		}
+
+		public void AddRangeIfNotNull_TestException() {
+			// Arrange
+			List<object> list = null;
+
+			// Test null source throws exception
+			Assert.ThrowsException<ArgumentNullException>(() => list.AddRangeIfNotNull(_itemsToAdd));
+		}
+
+		private static object[] _itemsToAdd = new object[] { null, 1, 2, "string" };
+		[TestMethod()]
+		public void AddRangeIfNotNull_TestAdding() {
+			// Arrange
+			var list = new List<object>();
+
+			// Test null item is not added.
+			var res = list.AddRangeIfNotNull(_itemsToAdd);
+			Assert.AreNotEqual(_itemsToAdd.Length, res);
+			Assert.AreEqual(3, res);
+
+			// Test enforceUnique: true => not added
+			res = list.AddRangeIfNotNull(_itemsToAdd, true);
+			Assert.AreEqual(0, res);
+
+			// Test enforceUnique: false => duplicates added
+			res = list.AddRangeIfNotNull(_itemsToAdd);
+			Assert.AreEqual(3, res);
+
+			// Make sure list count is now 6
+			Assert.AreEqual(6, list.Count);
+		}
+	}
+}
---
+++ Xrd.CommonTests/Collections/PaginatedResultsTests.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xrd.Collections;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xrd.Collections.Tests {
+	internal class TestPaginatedResults : PaginatedResults {
+		private bool _canSearch;
+		internal TestPaginatedResults(bool canSearch = false) : base() {
+			_canSearch = canSearch;
+		}
+		public override bool CanSearch => _canSearch;
+		protected async override Task<bool> PerformWork() {
+			await Task.Delay(10);
+			return true;
+		}
+
+		internal static TestPaginatedResults UninitializedResults =>
+			new TestPaginatedResults(false);
+		internal static TestPaginatedResults InitializedButNoResults =>
+			new TestPaginatedResults(true);
+		internal static TestPaginatedResults InitializedWithResults =>
+			new TestPaginatedResults(true) {
+				TotalItems = 100,
+				PageSize = 25,
+				PageIndex = 1
+			};
+	}
+	[TestClass()]
+	public class PaginatedResultsTests {
+		[TestMethod()]
+		public void CanSearch_TestWhenUnready() {
+			// Arrange
+			TestPaginatedResults set = TestPaginatedResults.UninitializedResults;
+
+			// Assert
+			Assert.IsFalse(set.CanSearch);
+		}
+
+		[TestMethod()]
+		public void CanSearch_TestWhenReady() {
+			TestPaginatedResults set = TestPaginatedResults.InitializedButNoResults;
+
+			Assert.IsTrue(set.CanSearch);
+		}
+
+		[TestMethod()]
+		public void CanMoveBack_TestWhenUnready() {
+			// Arrange
+			TestPaginatedResults set = TestPaginatedResults.UninitializedResults;
+
+			Assert.IsFalse(set.CanMoveBack);
+		}
+
+		[TestMethod()]
+		public void CanMoveBack_TestWhenSearchable() {
+			var set = TestPaginatedResults.InitializedButNoResults;
+
+			Assert.IsFalse(set.CanMoveBack);
+		}
+
+		[TestMethod()]
+		public void CanMoveBack_TestWhenReadyPage1() {
+			var set = TestPaginatedResults.InitializedWithResults;
+
+			Assert.IsFalse(set.CanMoveBack);
+		}
+
+		[TestMethod()]
+		public void CanMoveBack_TestWhenReadyPage2() {
+			var set = TestPaginatedResults.InitializedWithResults;
+
+			set.PageIndex = 3;
+
+			Assert.IsTrue(set.CanMoveBack);
+		}
+
+		[TestMethod()]
+		public async Task MoveFirst_TestWhenPage1() {
+			var set = TestPaginatedResults.InitializedWithResults;
+			Assert.IsFalse(await set.MoveFirst());
+		}
+
+		[TestMethod()]
+		public async Task MoveFirst_TestWhenPage2() {
+			var set = TestPaginatedResults.InitializedWithResults;
+			set.PageIndex = 2;
+			Assert.IsTrue(await set.MoveFirst());
+		}
+
+		[TestMethod()]
+		public async Task MovePrevious_Test() {
+			var set = TestPaginatedResults.InitializedWithResults;
+			set.PageIndex = 2;
+			Assert.IsTrue(await set.MovePrevious());
+		}
+
+		[TestMethod()]
+		public async Task JumpToPageTest() {
+			var set = TestPaginatedResults.InitializedWithResults;
+			Assert.IsTrue(await set.JumpToPage(3));
+		}
+
+		[TestMethod()]
+		public async Task MoveNextTest() {
+			var set = TestPaginatedResults.InitializedWithResults;
+			Assert.IsFalse(await set.MoveFirst());
+			Assert.IsTrue(await set.MoveNext());
+		}
+
+		[TestMethod()]
+		public async Task MoveLastTest() {
+			var set = TestPaginatedResults.InitializedWithResults;
+			Assert.IsTrue(await set.MoveLast());
+		}
+
+		[TestMethod()]
+		public async Task MoveFirstTest() {
+			var set = TestPaginatedResults.InitializedWithResults;
+			set.PageIndex = 3;
+			Assert.IsTrue(await set.MoveFirst());
+		}
+	}
+}
