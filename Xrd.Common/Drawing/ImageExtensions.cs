0904d996c5a1c90f542e2f14279a2ca992b0b507
John Hill <hilldata@hotmail.com>
2020-03-28 11:47:06 -0400

    Added Windows Shell API wrapper Xrd.Drawing.WindowsThumbnailProvider Added ImageExtensions to convert Images to/from binary data (bits)

---
+++ Xrd.Common/Drawing/ImageExtensions.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Xrd.Drawing {
+	/// <summary>
+	/// Class containing extension methods to easily convert 
+	/// <see cref="Image"/>s to/from binary data (byte arrays) 
+	/// and to create downsized thumbnails.
+	/// </summary>
+	public static class ImageExtensions {
+		// Share the memory stream across functions to prevent early disposal.
+		private static MemoryStream ms;
+		/// <summary>
+		/// Convert binary data (byte array) into <see cref="Image"/>
+		/// using a MemoryStream
+		/// </summary>
+		/// <param name="imageData">The image's binary data.</param>
+		/// <returns>An <see cref="Image"/></returns>
+		public static Image CreateImage(this byte[] imageData) {
+			if (imageData.IsNullOrEmpty())
+				throw new ArgumentNullException(nameof(imageData));
+
+			ms = new MemoryStream(imageData) {
+				Position = 0
+			};
+			return Image.FromStream(ms);
+		}
+
+		/// <summary>
+		/// Get the binary data (byte array) for the specified 
+		/// <see cref="Image"/>
+		/// </summary>
+		/// <param name="image">The image to read into binary data.</param>
+		/// <returns>The binary data that comprises the <see cref="Image"/></returns>
+		public static byte[] GetImageBinaryData(this Image image) {
+			ms = new MemoryStream();
+			image.Save(ms, ImageFormat.Jpeg);
+			ms.Position = 0;
+			return ms.ToArray();
+		}
+
+		// Image.GetThumbnailImageAbort delegate.
+		private static bool ThumbnailCallback() => false;
+
+		/// <summary>
+		/// Generate a thumbnail from an <see cref="Image"/> and return
+		/// it as an <see cref="Image"/>
+		/// </summary>
+		/// <param name="source">
+		/// The source image to downsize into a thumbnail
+		/// </param>
+		/// <param name="maxDim">
+		/// The maximum dimension (height or width) for the resulting thumbnail.
+		/// </param>
+		/// <returns>The resulting thumbnail as an <see cref="Image"/></returns>
+		public static Image CreateThumbnail(this Image source, int maxDim = 120) {
+			// Initialize thumbnail dimensions to the maxDim parameter.
+			int tH = maxDim;
+			int tW = maxDim;
+
+			// If the source image is not square, scale the shorter 
+			// thumbnail dimension to preserve the aspect ratio.
+			if(source.Height != source.Width) {
+				if(source.Height > source.Width) {
+					tW = tW * source.Width / source.Height;
+				}else {
+					tH = tH * source.Height / source.Width;
+				}
+			}
+
+			Image.GetThumbnailImageAbort callback = new Image.GetThumbnailImageAbort(ThumbnailCallback);
+			return source.GetThumbnailImage(tW, tH, callback, IntPtr.Zero);
+		}
+
+		/// <summary>
+		/// Generate a thumbnial from an image's binary data and return
+		/// it as an <see cref="Image"/>
+		/// </summary>
+		/// <param name="imageData">The binary data for the image.</param>
+		/// <param name="maxDim">
+		/// The maximum dimension (height or width) for the resulting thumbnail.
+		/// </param>
+		/// <returns>The resulting thumbnail as an <see cref="Image"/></returns>
+		public static Image CreateThumbnail(this byte[] imageData, int maxDim = 120) =>
+			imageData.CreateImage().CreateThumbnail(maxDim);
+
+		/// <summary>
+		/// Generate a thumbnail from an image's binary data and return
+		/// the resulting thumbnail's binary data (byte array).
+		/// </summary>
+		/// <param name="imageData">The binary data for the image.</param>
+		/// <param name="maxDim">
+		/// The maximum dimension (height or width) for the resulting thumbnail
+		/// </param>
+		/// <returns>The result thumbnail's binary data (byte array).</returns>
+		public static byte[] GetThumbnailData(this byte[] imageData, int maxDim) =>
+			imageData.CreateImage().CreateThumbnail(maxDim).GetImageBinaryData();
+	}
+}
---
+++ Xrd.Common/Drawing/ThumbnailOptions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Xrd.Drawing {
+	/// <summary>
+	/// Enumeration of the thumbnail options used by the
+	/// <see cref="WindowsThumbnailProvider"/> class.
+	/// </summary>
+	/// <remarks>
+	/// H/T https://github.com/rlv-dan/ShellThumbs
+	/// IShellItemImageFactory Flags: https://docs.microsoft.com/en-us/windows/win32/api/shobjidl_core/nf-shobjidl_core-ishellitemimagefactory-getimage?redirectedfrom=MSDN
+	/// </remarks>
+	[Flags]
+	public enum ThumbnailOptions {
+		/// <summary>
+		/// Shrink the bitmap as necessary to fit, preserving the aspect ratio.
+		/// </summary>
+		None = 0x00,
+		/// <summary>
+		/// Allows stretching of the source image to create a larger thumbnail.
+		/// </summary>
+		BiggerSizeOk = 0x01,
+		/// <summary>
+		/// Return the item only if it is already in memory.
+		/// </summary>
+		InMemoryOnly = 0x02,
+		/// <summary>
+		/// Return only the file type's icon, never a thumbnail.
+		/// </summary>
+		IconOnly = 0x04,
+		/// <summary>
+		/// Return only the thumbnail, never an icon. Note that since not all
+		/// items have thumbnails, this flag will cause the method to fail if 
+		/// not thumbnail can be generated.
+		/// </summary>
+		ThumbnailOnly = 0x08,
+		/// <summary>
+		/// Allows access to disk, but only to retrieve a cached item.
+		/// </summary>
+		InCacheOnly = 0x10,
+		/// <summary>
+		/// Intro'ed in Win8. Crop the bitmap to a square.
+		/// </summary>
+		CropToSquare_Win8 = 0x20,
+		/// <summary>
+		/// Intro'ed in Win8. Stretch and crop to 0.7 aspect ratio
+		/// </summary>
+		WideThumbnail_Win8 = 0x40,
+		/// <summary>
+		/// Intro'ed in Win8. If returning an icon, paint a background using
+		/// the associated app's registered background color.
+		/// </summary>
+		IconBackground_Win8 = 0x80,
+		/// <summary>
+		/// Intro'ed in Win8. If necessary, stretch bitmap so height and width
+		/// fit the given size.
+		/// </summary>
+		ScaleUp_Win8 = 0x100,
+	}
+}
